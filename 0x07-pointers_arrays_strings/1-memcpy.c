97e7137e5fbbee74b15181ff24e102461f8e6628
semoabd <muhammedunisimak@gmail.com>
2023-01-03 11:03:42 -0800

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,7 +3,7 @@
  * _memcpy - fills memory with another buffer.
  * @dest: source string
  * @src: string for filling 
- * @n: length of buffer 
+ * @n: lenght of buffer 
  * Return: new string.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
